8b4f7444333b97c3d0976cc9fd4aa2a1c9f58816
soumya-15-2005-byte <soumyagupta.985@gmail.com>
2026-08-12 13:55:38 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = require('./api/index');
-
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
-}
-
-module.exports = app;
